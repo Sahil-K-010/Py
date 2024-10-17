b04afb6eb27f47ff3cf96915de1660e8be3139f8
Sahil-K-010 <127997272+Sahil-K-010@users.noreply.github.com>
2024-10-17 13:00:43 +0530

    Add files via upload

---
+++ 17-Oct.py
@@ -0,0 +1,11 @@
+import streamlit as st
+st.title('Loan Application App')
+l=st.number_input('Enter your amount for loan')
+s=st.number_input('Enter your salary')
+c=st.number_input('Enter your credit score')
+
+if s>=40000 and c>=500:
+    st.write('Congratulations')
+
+else:
+    st.write('We are sorry')
